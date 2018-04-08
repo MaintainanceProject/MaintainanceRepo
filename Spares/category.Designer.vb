9f998497404e705e3702e21954aaef2009add124
oum <kodchakonpukdee@gmail.com>
2018-04-08 16:10:57 +0700

    Add project files.

---
+++ Spares/category.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class category
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.namecategory = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.categoryTable = New System.Windows.Forms.DataGridView()
+        CType(Me.categoryTable, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'namecategory
+        '
+        Me.namecategory.Location = New System.Drawing.Point(68, 31)
+        Me.namecategory.Name = "namecategory"
+        Me.namecategory.Size = New System.Drawing.Size(161, 20)
+        Me.namecategory.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(271, 28)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'categoryTable
+        '
+        Me.categoryTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.categoryTable.Location = New System.Drawing.Point(13, 108)
+        Me.categoryTable.Name = "categoryTable"
+        Me.categoryTable.Size = New System.Drawing.Size(704, 182)
+        Me.categoryTable.TabIndex = 2
+        '
+        'category
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(729, 302)
+        Me.Controls.Add(Me.categoryTable)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.namecategory)
+        Me.Name = "category"
+        Me.Text = "category"
+        CType(Me.categoryTable, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents namecategory As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents categoryTable As DataGridView
+End Class
---
+++ Spares/category.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Data.SqlClient
+Public Class category
+
+    Dim opendb As New SqlConnection("Server = DESKTOP-UOMVELS\SQLEXPRESS; Database = testdb; Integrated Security = true")
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim insertExecute As String = "INSERT INTO dbo.category (category, created_date, updated_date) VALUES ('" & namecategory.Text & "', GETUTCDATE(), GETUTCDATE())"
+        ExecuteNonQuery(insertExecute)
+        Dim dTable As DataTable = ExecuteQuery("select * from category")
+        categoryTable.DataSource = dTable
+    End Sub
+
+
+    Public Sub ExecuteNonQuery(query As String)
+        Dim command As New SqlCommand(query, opendb)
+        opendb.Open()
+        command.ExecuteNonQuery()
+        opendb.Close()
+    End Sub
+
+
+    Public Function ExecuteQuery(query As String) As DataTable
+        Dim command As New SqlCommand(query, opendb)
+
+        Dim adapter As New SqlDataAdapter(command)
+        Dim table As New DataTable()
+        adapter.Fill(table)
+        Return table
+        'userView.DataSource = table
+    End Function
+
+End Class
